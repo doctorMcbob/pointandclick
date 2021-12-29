b77aee77947eecdee36e88fa6c04cd8cd22987ed
doctorMcbob <wesley@abe.ai>
2021-12-29 11:52:46 -0800

    inventory button

--- src/game.py
+++ src/game.py
@@ -1,8 +1,9 @@
 import pygame
+from pygame import Surface, Rect
 
 from src.utils import expect_click
 
-IMG_LOCATION="/img/"
+IMG_LOCATION="src/img/"
 
 TITLE="Point And Click Adventure Game Jam"
 
@@ -10,7 +11,7 @@ STARTING_ROOM = "ROOT"
 
 ROOMS = {
     "ROOT" : {
-        "IMG"    : pygame.Surface((1080, 640)),
+        "IMG"    : None,
         "ITEMS"  : {},
         "ACTORS" : {},
         "LOCKS"  : {},
@@ -20,8 +21,18 @@ ROOMS = {
 
 SHOW_INV = False
 
-ROOMS[STARTING_ROOM]["IMG"].fill((255, 255, 255))
-ROOM_SPRITESHEET = {}
+ROOM_SPRITESHEET = {
+    "ROOT":((0, 0), (1080, 640))
+}
+UX_SPRITESHEET = {
+    "INVBUTTON":((0, 0), (64, 64)),
+    "INVSLOT": ((256, 0) , (256, 48)),
+    
+}
+
+BUTTONS = {
+    "INVBUTTON": ((1080-64, 640 - 64), (64, 64))
+}
 
 def load_spritesheet(filename, data, colorkey=(1, 255, 1)):
     """data should be dict with key: ((x, y), (w, h)), assumes w, h are 32, 32"""
@@ -42,16 +53,20 @@ def draw(G, mouse_pos=None):
         pos, dim = piece
         G["SCREEN"].blit(piece["IMG"], pos)
 
+    pos, dim = BUTTONS["INVBUTTON"]
+    G["SCREEN"].blit(G["SYSIMG"]["INVBUTTON"], pos)
     if SHOW_INV:
         idx = mouse_pos[1] // 48 if mouse_pos is not None and mouse_pos[0] > 1080-256 else None
             
         G["SCREEN"].blit(drawn_inventory(G, idx), (1080 - 256, 0))
 
+
 def drawn_inventory(G, idx=None):
     inv = G["INV"]
-    surf = pygame.Surface((256, 640))
+    surf = pygame.Surface((256, 560))
     surf.fill((255, 255, 255))
-    # opportunity to draw sprite here
+    for i in range(10):
+        surf.blit(G["SYSIMG"]["INVSLOT"], (0, i * 48))
     for i, item in enumerate(inv):
         col = (0, 150, 0) if idx == i else (0, 0, 0)
         surf.blit(G["HEL32"].render(item, 0, (0, 0, 0)), (8, 8 + i * 48))
@@ -67,9 +82,10 @@ def setup_game():
     G["ROOM"] = STARTING_ROOM
 
     room_sprites = load_spritesheet("rooms.png", ROOM_SPRITESHEET)
+    G["SYSIMG"] = load_spritesheet("menu.png",  UX_SPRITESHEET)
     for room in ROOMS:
-        if room in room_spritesheet:
-            room["IMG"] = room_sprites[room]
+        if room in ROOM_SPRITESHEET:
+            ROOMS[room]["IMG"] = room_sprites[room]
     
     pygame.display.set_caption(TITLE)
     return G
@@ -81,9 +97,13 @@ def click_draw(G):
     pygame.draw.circle(G["SCREEN"], (100, 0, 0), pos, 2)
     
 def run_game(G):
+    global SHOW_INV
     while True:
         draw(G)
         pygame.display.update()
         pos = expect_click(G, click_draw)
+
+        if Rect(BUTTONS["INVBUTTON"]).collidepoint(pos):
+            SHOW_INV = not SHOW_INV
 
         
